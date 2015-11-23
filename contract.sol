69738da384b4ee4e0740de45aea03428fc300ef6
Jeffrey Wilcke <geffobscura@gmail.com>
2015-11-23 17:45:03 +0100

    Added contract functionality
    * Reclaim funds after time lock has expired
    * Fixed verify
    * Channels are now owned instead of contract being owned

--- contract.sol
+++ contract.sol
@@ -1,17 +1,25 @@
 contract Channel {
-	address public owner;
-	mapping(bytes32 => bool) public channels;
+	struct PaymentChannel {
+		address owner;
+		uint256 value;
+		uint validUntil;
+
+		bool valid;
+	}
+	mapping(bytes32 => PaymentChannel) public channels;
 	uint id;
 
+	event NewChannel(bytes32 indexed channel);
+	event Reclaim(bytes32 indexed channel);
+	event Deposit(address indexed who);
+
 	function Channel() {
-		owner = msg.sender;
 		id = 0;
 	}
 
-	event NewChannel(bytes32 indexed channel);
 	function createChannel() {
 		bytes32 channel = sha3(id++);
-		channels[channel] = true;
+		channels[channel] = PaymentChannel(msg.sender, msg.value, block.timestamp + 1 days, true);
 
 		NewChannel(channel);
 	}
@@ -23,23 +31,54 @@ contract Channel {
 
 	// verify a message (receipient || amount) with the provided signature
 	function verify(bytes32 channel, address recipient, uint amount, uint8 v, bytes32 r, bytes32 s) constant returns(bool) {
-		return channels[channel] && owner == ecrecover(getHash(channel, recipient, amount), v, r, s);
+		PaymentChannel ch = channels[channel];
+		return ch.valid && ch.validUntil > block.timestamp && ch.owner == ecrecover(getHash(channel, recipient, amount), v, r, s);
 	}
 
 	// claim funds
 	function claim(bytes32 channel, address recipient, uint amount, uint8 v, bytes32 r, bytes32 s) {
 		if( !verify(channel, recipient, amount, v, r, s) ) return;
 
-		// TODO checking for sufficient funds
-		recipient.send(amount);
+		PaymentChannel ch = channels[channel];
+		if( amount > ch.value ) {
+			recipient.send(ch.value);
+			ch.value = 0;
+		} else {
+			recipient.send(amount);
+			ch.value -= amount;
+		}
 
-		delete channels[channel];
+		// channel is no longer valid
+		channels[channel].valid = false;
+	}
+
+	// reclaim a channel
+	function reclaim(bytes32 channel) {
+		PaymentChannel ch = channels[channel];
+		if( ch.value > 0 && ch.validUntil < block.timestamp ) {
+			ch.owner.send(ch.value);
+			delete channels[channel];
+		}
+	}
+
+	function getChannelValue(bytes32 channel) constant returns(uint256) {
+		return channels[channel].value;
+	}
+
+	function getChannelOwner(bytes32 channel) constant returns(address) {
+		return channels[channel].owner;
+	}
+
+	function  getChannelValidUntil(bytes32 channel) constant returns(uint) {
+		return channels[channel].validUntil;
+	}
+	function isValidChannel(bytes32 channel) constant returns(bool) {
+		PaymentChannel ch = channels[channel];
+		return ch.valid && ch.validUntil >= block.timestamp;
 	}
 
 	// deposit
-	event Deposit(address indexed who);
 	function() {
 		Deposit(msg.sender);
 	}
 }
-
